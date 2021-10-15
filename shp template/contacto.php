82e06317a39da94960a165165a2760be33dfa9f9
Fiorela Avendaño <87959185+Fiorela-GitHub@users.noreply.github.com>
2021-10-15 12:59:44 -0300

    Add files via upload

---
+++ shp template/contacto.php
@@ -0,0 +1,44 @@
+<section class="contacto-section" id="contacto">
+            <div class="container">
+              <div class="row mx-auto text-center">
+                <div class="col-lg-8 mx-auto">
+                    <h3 class="mb-4">contacto</h3>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-8 col-12 mx-auto">
+                <form action="post.php" method="post">
+                  <div class="row mb-3">
+                    <div class="col">
+                      <input type="text" class="form-control" name="nombre" placeholder="Nombre" id="name">
+                    </div>
+                    <div class="col">
+                      <input type="text" class="form-control" name="apellido" placeholder="Apellido" id="lastname">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <div class="col">
+                      <input type="text" class="form-control" name="telefono" placeholder="Teléfono" id="phone">
+                    </div>
+                    <div class="col">
+                      <input type="text" class="form-control" name="email" placeholder="Email" id="email">
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <div class="col">
+                      <textarea type="text" class="form-control" name="mensaje" placeholder="Mensaje" id="message"></textarea>
+                    </div>
+                  </div>
+                  <div class="row mb-3">
+                    <div class="col">
+                    </div>
+                    <div class="col">
+                      <button type="submit" class="btn btn-enviar">Enviar</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              </div>
+            </div>
+            <footer class="footer small text-center"><div class="container">Plantilla web didáctica - Potrero Digital 2021</div></footer>
+        </section>
